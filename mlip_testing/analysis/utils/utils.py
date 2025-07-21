10102d973fa4c2cfedaa2e01586b019efa878206
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-07-21 16:21:53 +0100

    Add app utils

--- mlip_testing/analysis/utils/utils.py
+++ mlip_testing/analysis/utils/utils.py
@@ -2,10 +2,6 @@
 
 from __future__ import annotations
 
-import json
-from pathlib import Path
-
-from dash.dash_table import DataTable
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 
@@ -45,27 +41,3 @@ def rmse(ref: list, prediction: list) -> float:
         Root mean squared error.
     """
     return mean_squared_error(ref, prediction)
-
-
-def rebuild_table(filename: str | Path) -> DataTable:
-    """
-    Rebuild saved dash table.
-
-    Parameters
-    ----------
-    filename
-        Name of json file with saved table data.
-
-    Returns
-    -------
-    DataTable
-        Dash DataTable.
-    """
-    # Load JSON file
-    with open(filename) as f:
-        table_json = json.load(f)
-
-    data = table_json["data"]
-    columns = table_json["columns"]
-
-    return DataTable(data=data, columns=columns, editable=True)
