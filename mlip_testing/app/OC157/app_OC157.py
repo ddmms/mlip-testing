140ec3434df9386947621d68efcb0016af10c0f0
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-07-29 17:54:56 +0100

    Update OC157 app and generalise assets

--- mlip_testing/app/OC157/app_OC157.py
+++ mlip_testing/app/OC157/app_OC157.py
@@ -5,8 +5,9 @@ from __future__ import annotations
 from pathlib import Path
 
 from dash import Dash
-from dash.html import H1, Div
+from dash.html import Div
 
+from mlip_testing.app.utils.build import layout_builder
 from mlip_testing.app.utils.callbacks import plot_from_table_column, struct_from_scatter
 from mlip_testing.app.utils.load import read_plot, rebuild_table
 from mlip_testing.calcs.models.models import MODELS
@@ -17,8 +18,9 @@ DATA_PATH = Path(__file__).parent.parent / "data" / "OC157"
 metrics_table = rebuild_table(DATA_PATH / "oc157_metrics_table.json", id="table")
 scatter = read_plot(DATA_PATH / "figure_rel_energies.json", id="figure")
 structs_dir = DATA_PATH / list(MODELS.keys())[0]
+# Assets dir will be parent directory
 structs = [
-    f"assets/{list(MODELS.keys())[0]}/{i}.xyz"
+    f"assets/OC157/{list(MODELS.keys())[0]}/{i}.xyz"
     for i in range(len(list(structs_dir.glob("*.xyz"))))
 ]
 
@@ -33,13 +35,10 @@ def build_layout() -> Div:
         Div component with list all components for app.
     """
     # Define all components/placeholders
-    return Div(
-        [
-            H1("OC157 benchmark", style={"color": "black"}),
-            Div(metrics_table),
-            Div(id="figure-placeholder"),
-            Div(id="struct-placeholder"),
-        ]
+    return layout_builder(
+        title="OC157 benchmark",
+        table=metrics_table,
+        extra_components=[Div(id="figure-placeholder"), Div(id="struct-placeholder")],
     )
 
 
@@ -56,7 +55,7 @@ def register_callbacks(app: Dash) -> None:
         app=app,
         table_id="table",
         plot_id="figure-placeholder",
-        column_to_plot={"Mean Absolute Error": scatter, "Ranking Error": scatter},
+        column_to_plot={"MAE (meV)": scatter, "Ranking Error": scatter},
     )
 
     struct_from_scatter(
@@ -69,7 +68,7 @@ def register_callbacks(app: Dash) -> None:
 
 if __name__ == "__main__":
     # Create and run Dash app
-    app = Dash(__name__, assets_folder=DATA_PATH)
+    app = Dash(__name__, assets_folder=DATA_PATH.parent)
     app.layout = build_layout()
     register_callbacks(app)
     app.run(port=8051, debug=True)
