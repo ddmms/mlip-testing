9297fa827ac2d56e0777a4199aebaf094c7d62b1
joehart2001 <jh2536@cam.ac.uk>
2025-08-21 22:54:49 +0100

    added S24 calc, analysis and app, added surface directory

--- mlip_testing/app/surfaces/S24/app_S24.py
+++ mlip_testing/app/surfaces/S24/app_S24.py
@@ -1,4 +1,4 @@
-"""Run OC157 app."""
+"""Run S24 app."""
 
 from __future__ import annotations
 
@@ -6,7 +6,6 @@ from pathlib import Path
 
 from dash import Dash
 from dash.html import Div
-import numpy as np
 
 from mlip_testing.app.base_app import BaseApp
 from mlip_testing.app.utils.build_callbacks import (
@@ -16,31 +15,27 @@ from mlip_testing.app.utils.build_callbacks import (
 from mlip_testing.app.utils.load import read_plot
 from mlip_testing.calcs.models.models import MODELS
 
-DATA_PATH = Path(__file__).parent.parent / "data" / "OC157"
+DATA_PATH = Path(__file__).parent.parent.parent / "data" / "surfaces" / "S24"
 
-SCATTER = read_plot(DATA_PATH / "figure_rel_energies.json", id="figure")
+SCATTER = read_plot(DATA_PATH / "figure_adsorption_energies.json", id="figure")
 STRUCTS_DIR = DATA_PATH / list(MODELS.keys())[0]
 # Assets dir will be parent directory
-STRUCTS = list(
-    np.repeat(
-        [
-            f"assets/OC157/{list(MODELS.keys())[0]}/{i}.xyz"
-            for i in range(len(list(STRUCTS_DIR.glob("*.xyz"))))
-        ],
-        3,
-    )
-)
+STRUCTS = [
+    f"assets/S24/{list(MODELS.keys())[0]}/{struct_file.stem}.xyz"
+    for struct_file in STRUCTS_DIR.glob("*.xyz")
+    if struct_file.name != "s24_mol_surface_atoms.xyz"
+]
 
 
-class OC157App(BaseApp):
-    """OC157 benchmark app layout and callbacks."""
+class S24App(BaseApp):
+    """S24 benchmark app layout and callbacks."""
 
     def register_callbacks(self) -> None:
         """Register callbacks to app."""
         plot_from_table_column(
             table_id=self.table_id,
             plot_id="figure-placeholder",
-            column_to_plot={"MAE": SCATTER, "Ranking Error": SCATTER},
+            column_to_plot={"MAE": SCATTER},
         )
 
         struct_from_scatter(
@@ -50,23 +45,23 @@ class OC157App(BaseApp):
         )
 
 
-def get_app() -> OC157App:
+def get_app() -> S24App:
     """
-    Get OC157 benchmark app layout and callback registration.
+    Get S24 benchmark app layout and callback registration.
 
     Returns
     -------
-    OC157App
+    S24App
         Benchmark layout and callback registration.
     """
-    return OC157App(
-        name="OC157",
-        title="OC157",
+    return S24App(
+        name="S24",
+        title="S24",
         description=(
-            "Performance in predicting relative energies between 3 structures for 157 "
+            "Performance in predicting adsorption energies for 24 "
             "molecule-surface combinations."
         ),
-        table_path=DATA_PATH / "oc157_metrics_table.json",
+        table_path=DATA_PATH / "s24_metrics_table.json",
         extra_components=[Div(id="figure-placeholder"), Div(id="struct-placeholder")],
     )
 
@@ -76,9 +71,9 @@ if __name__ == "__main__":
     full_app = Dash(__name__, assets_folder=DATA_PATH.parent)
 
     # Construct layout and register callbacks
-    oc157_app = get_app()
-    full_app.layout = oc157_app.layout
-    oc157_app.register_callbacks()
+    s24_app = get_app()
+    full_app.layout = s24_app.layout
+    s24_app.register_callbacks()
 
     # Run app
-    full_app.run(port=8051, debug=True)
+    full_app.run(port=8052, debug=True)
